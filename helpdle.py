b10041826a239194b5370b10d1789aaab8616e0e
brendan obrien <bobrien.516@gmail.com>
2024-07-10 21:00:58 -0400

    add support for negative positions

--- helpdle.py
+++ helpdle.py
@@ -1,10 +1,11 @@
 from colorama import Fore, Style
+from collections import defaultdict
 import sys
 
 def read_dictionary():
     # Path to the system dictionary on macOS
     dictionary_path = '/usr/share/dict/words'
-    #dictionary_path = '/tmp/dict/words'
+   
 
     # Read words from the dictionary file
     with open(dictionary_path, 'r') as file:
@@ -13,29 +14,23 @@ def read_dictionary():
     return words
 
 def colorize_word(word, required_letters, known_positions):
-    colored_word = ""
-    print(f"colorizing word {word}")
-    print(known_positions)
-    print(required_letters)
+    colorized_word = ""
+    '''
+    loop through lettters in word
+    if letter is in required letters highlight yellow
+    if letter is in required letters and at specified position highlight green
+    '''
     for i, letter in enumerate(word):
-    #    print(f"i: {i}, letter: {letter}")
-    #    print(f"get: {known_positions.get(str(i+1))}")
         if letter in required_letters:
             if known_positions.get(str(i + 1)) and word[i] == known_positions[str(i+1)]:
-                colored_word += Fore.GREEN + letter + Style.RESET_ALL
+                colorized_word += Fore.GREEN + letter + Style.RESET_ALL
             else:
-                colored_word += Fore.YELLOW + letter + Style.RESET_ALL
+                colorized_word += Fore.YELLOW + letter + Style.RESET_ALL
         else:
-            colored_word += letter
-    return colored_word
+            colorized_word += letter
+    return colorized_word
 
-'''
-loop through lettters in word
-if letter is in required letters highlight yellow
-if letter is in required letters and at specified position highlight green
-'''
-
-def search_words_by_criteria(words, required_letters, exclude_letters, known_positions):
+def search_words_by_criteria(words, required_letters, exclude_letters, known_positions,negative_positions):
     # Filter words based on the criteria
     filtered_words = []
 
@@ -47,6 +42,12 @@ def search_words_by_criteria(words, required_letters, exclude_letters, known_pos
             if word_lower[int(position) - 1] != letter:
                 match = False
                 break
+        
+        if match:
+            for position, letters in negative_positions.items():
+                if word_lower[int(position) - 1] in letters:
+                    match = False
+                    break
         
         if match:
             for letter in required_letters:
@@ -71,16 +72,18 @@ def main():
 
     while True:
         required_letters = set()
-        known_positions = {}
         exclude_letters = set()
+        known_positions = {}
+        negative_positions = defaultdict(set)
         
         while True:
             print("\nMenu:")
             print("1. Add required letter")
             print("2. Specify known position")
-            print("3. Add exclude letter")
-            print("4. Perform search")
-            print("5. Exit")
+            print("3. Specify negative position")
+            print("4. Add exclude letter")
+            print("5. Perform search")
+            print("6. Exit")
 
             choice = input("Enter your choice: ")
 
@@ -93,7 +96,10 @@ def main():
                         break
                     for letter in letters:
                         if letter.isalpha():
-                            required_letters.add(letter.lower())
+                            if letter not in exclude_letters:
+                                required_letters.add(letter.lower())
+                            else:
+                                print(f"letter {letter} in exclude list, cannot be required")
                         else:
                             print(f"invalid required letter {letter}")
 
@@ -112,32 +118,54 @@ def main():
                                 known_positions[position] = letter.lower()
                         except ValueError:
                             print(f"invalid position {position}, must be between 1 and 5")
-                    
-                #TODO support negative positions - e.g. it is known that a is not in position 1
             
             if choice == '3':
+                l2p = input("Enter the letter and a position it cannot occupy (1 to 5) separated by a colon, e.g. 'a:1': ")
+                if not l2p:
+                    break
+                else:
+                    (letter,position) = l2p.split(':')
+                    if not letter.isalpha():        
+                        print(f"invalid letter: {letter}")
+                    else:
+                        required_letters.add(letter.lower())
+                        try:
+                            if 1 <= int(position) <= 5:
+                                negative_positions[position].add(letter.lower())
+                        except ValueError:
+                            print(f"invalid position {position}, must be between 1 and 5")
+            
+            if choice == '4':
                 exclude_letter_input = input("Enter one or more exclude letters separated by space (or press Enter to skip): ")
                 if not exclude_letter_input:
                     break
-                for el in exclude_letter_input.split():
-                    exclude_letters.add(el.lower())
+                for letter in exclude_letter_input.split():
+                        if letter.isalpha():
+                            if letter not in required_letters:
+                                exclude_letters.add(letter.lower())
+                            else:
+                                print(f"letter {letter} in required list, cannot be excluded")
+                        else:
+                            print(f"invalid required letter {letter}")
+
             
-            if choice == '4':
-                filtered_words = search_words_by_criteria(words if not filtered_words else filtered_words, required_letters, exclude_letters,known_positions)
+            if choice == '5':
+                filtered_words = search_words_by_criteria(words if not filtered_words else filtered_words, required_letters, exclude_letters,known_positions,negative_positions)
 
                 if filtered_words:
-                    print(f"Found {len(filtered_words)} words matching search criteria:")
-                    if len(filtered_words) < 20:
+                    display = input(f"Found {len(filtered_words)} words matching search criteria. Display them? (y/n)")
+                    if display in ('Y','y'): 
                         for word in filtered_words:
-                            print(word)
-                            colored_word = colorize_word(word, required_letters,known_positions)
-                            print(colored_word)
+                            #print(word)
+                            colorized_word = colorize_word(word, required_letters,known_positions)
+                            print(colorized_word)
                     
                 else:
-                    print(f"No words found matching the criteria.")
+                    print(f"No words found matching the criteria:")
+                    print(f"required_letters: {required_letters}")
                     sys.exit(0)
 
-            if choice == '5':
+            if choice == '6':
                 sys.exit(0)
 
 if __name__ == "__main__":
